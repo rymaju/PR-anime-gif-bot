96f58dd8530db6b64f7724b9416bbda1b8b7a1f0
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-03-23 23:18:18 -0400

    init

---
+++ src/gifs.ts
@@ -0,0 +1,16 @@
+const GIFS: string[] = [
+    "https://camo.githubusercontent.com/a67614ef331a93697b635700ef439fe759fd6aa07d36feed3c5ab8437fd098d4/68747470733a2f2f6d65646961312e74656e6f722e636f6d2f696d616765732f64346637643066376166653839383232653334393265626135383263353937362f74656e6f722e676966",
+    "https://media1.tenor.com/images/53e00327a221637f76bdb3d20e4568a0/tenor.gif",
+    "https://media1.tenor.com/images/3d2574a66760415d655fdd6f5e57c044/tenor.gif",
+    "https://media1.tenor.com/images/bddf2872f32eea960e56bd76093dd3c9/tenor.gif",
+    "https://gifimage.net/wp-content/uploads/2017/07/anime-celebration-gif-14.gif",
+    "https://media.giphy.com/media/Te3vyhjhlQBaM/giphy.gif",
+    "https://i.gifer.com/VEK0.gif",
+    "https://anerdyperspective.files.wordpress.com/2019/03/img_1817.gif",
+    "https://media3.giphy.com/media/11NlXRhFSZl4w8/giphy.gif",
+    "https://averagejoe351213450.files.wordpress.com/2020/07/tenor-1.gif",
+    "https://data.whicdn.com/images/220999505/original.gif",
+    "https://media0.giphy.com/media/pWsdECIr4w9Jm/giphy.gif"
+]
+
+export default GIFS
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { Probot } from "probot";
-
+import GIFS from './gifs'
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
+  app.on("pull_request.closed", async (context) => {
+    const randomNum = Math.floor(Math.random() * GIFS.length)
+    console.log(randomNum)
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: `<img src="${GIFS[randomNum]}" width="600"/>`,
     });
+    console.log('creating a comment...')
+    
     await context.octokit.issues.createComment(issueComment);
   });
   // For more information on building apps:
